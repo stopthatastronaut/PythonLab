cff69239fdc76c848095761ae547d639b038ce03
jasbro <jasbro@jasbro.io>
2025-11-04 16:47:51 +1100

    some stuff

--- data_structures/structures.py
+++ data_structures/structures.py
@@ -1,14 +1,24 @@
+import random
+
 # use built-in data structures
+# https://www.geeksforgeeks.org/python/python-data-structures/ is a good starting point
 
 class structures:
     """
     This one plays with and returns a bunch of data structures for the LOLs
     """
+
+
     adict = { 'jason': 51, 'dave': 42 }
     alist = [1,2,3,4,5,6]
     list1 = [1, 2, 4, 5, 6]
     print("\nTuple using List: ")
     Tuple = tuple(list1)
+
+
+    print("\nRandom from List: ")
+
+    print(random.choice(list1))
 
 
 
@@ -29,3 +39,6 @@ class structures:
     # Checking the element
     # using in keyword
     print("Is" in aSet)
+
+    # https://www.geeksforgeeks.org/python/deque-in-python/
+    # deques
